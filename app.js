650eca7f92b817f0a905f2de01ea5cf36dc22e19
Bryan <bryan.fennell@hotmail.com>
2020-04-27 16:08:26 -0400

    Defined classes. All tests pass.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const
+  Manager = require("./lib/Manager"),
+  Engineer = require("./lib/Engineer"),
+  Intern = require("./lib/Intern"),
+  inquirer = require("inquirer"),
+  path = require("path"),
+  fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const
+  OUTPUT_DIR = path.resolve(__dirname, "output"),
+  outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
