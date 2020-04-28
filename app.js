8d357fb2cc49f6584a3c31d92261455531ed88cb
Bryan <bryan.fennell@hotmail.com>
2020-04-27 20:55:47 -0400

    Cleaned up and documented code. Added some light styling to the output.

--- app.js
+++ app.js
@@ -13,10 +13,18 @@ const
 
 let employees = []
 
+
+/*
+  Program entry point.
+ */
 function init() {
   promptEmployeeType()
 }
 
+
+/*
+  Ask what type of employee we should add.
+ */
 function promptEmployeeType() {
   inquirer.prompt([
     {
@@ -35,27 +43,35 @@ function promptEmployeeType() {
 }
 
 
+/*
+  Create the list of questions for the specified employee type and then
+  ask the user.
+ */
 function promptEmployeeInfo(employeeType) {
   const prompts = questions["Common"].slice()
   prompts.push(questions[employeeType])
 
   inquirer.prompt(prompts).then(response => {
-      let employee = employeeBuilder[employeeType](response)
-      employees.push(employee)
-      promptContinue()
+    const employee = employeeBuilder[employeeType](response)
+    employees.push(employee)
+    promptAddAnother()
   })
 }
 
 
-function promptContinue() {
+/*
+  Ask if we should add another employee. If so, begin the question sequence
+  again. If not, save the report.
+ */
+function promptAddAnother() {
   inquirer.prompt([
     {
-      message: "Would you like to add another employee (y/n)?",
-      name: "addAnother",
-      validate: validateYesNo
+      type: "confirm",
+      message: "Would you like to add another employee?",
+      name: "addAnother"
     }
   ]).then(({addAnother}) => {
-    if (addAnother.includes('y')) {
+    if (addAnother) {
       promptEmployeeType()
     } else {
       saveTeamReport(employees)
@@ -63,6 +79,11 @@ function promptContinue() {
   })
 }
 
+
+/*
+  Reneder the report from the list of employees and save it under the
+  output folder.
+ */
 function saveTeamReport(employees) {
   createFolder(OUTPUT_DIR)
 
@@ -74,8 +95,9 @@ function saveTeamReport(employees) {
   })
 }
 
+
 /*
-  Create a folder under the project directory if it does not exist.
+  Create a directory if it does not exist.
  */
 function createFolder(path) {
   if (!fs.existsSync(path)) {
@@ -116,7 +138,6 @@ const questions = {
     validate: validateMinimal
   },
   Manager: {
-    type: "number",
     message: "What is the employee's office number?",
     name: "officeNumber",
     validate: validatePositiveNumber
@@ -142,24 +163,13 @@ const employeeBuilder = {
 }
 
 
-// -- Prompt input validation functions ---
-
-/*
-  Validate a yes or no answer.
- */
-function validateYesNo(answer) {
-  return ['y', 'n', 'yes', 'no'].includes(answer.toLowerCase()) ?
-    true :
-    "Please answer yes or no."
-}
-
-
 /*
   For more unstructured questions, validate the user at least entered something.
  */
 function validateMinimal(answer) {
   return answer.trim().length ? true : "Please provide an answer."
 }
+
 
 /*
   Validate an integer of 0 or higher.
@@ -181,27 +191,5 @@ function validateEmailAddress(answer) {
     "Please provide a valid email address."
 }
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
 init();
